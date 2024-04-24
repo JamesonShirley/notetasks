c127e5802d1de3896c98d8da9b2301dc94d1fa8c
Jameson Shirley <jameson.shirley24@gmail.com>
2024-04-24 17:38:39 -0600

    Update settings.py

--- notetaker/notetaker/settings.py
+++ notetaker/notetaker/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')Run the command: py manage.py collectstatic to collect static files.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
